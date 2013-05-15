88169ae02c3ae3d813dae280ef38c52becbe84f4
rod156 <asteriskrodserv@gmail.com>
2013-05-15 14:12:20 -0400

    fdgd

---
+++ MediaPlayer/MediaPlayer/MainPage.xaml.vb
@@ -0,0 +1,13 @@
+﻿Partial Public Class MainPage
+    Inherits PhoneApplicationPage
+
+    ' Constructor
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+
+    Private Sub btnPlay_Click(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles btnPlay.Click
+        'This button will cause the Media Element to execute the song selected.
+    End Sub
+End Class
